ba79f4c61f98dbfc758c0079287733a4cabd818c
Andrii Shevchenko <ashevchenko0309@gmail.com>
2023-11-05 11:41:32 +0200

    rollback revalidation function

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -1,9 +1,8 @@
-// import { revalidate } from 'lib/shopify';
+import { revalidate } from 'lib/shopify';
 import { NextRequest, NextResponse } from 'next/server';
 
 export const runtime = 'edge';
 
 export async function POST(req: NextRequest, res: NextResponse): Promise<NextResponse> {
-    return res.json()
-  // return revalidate(req);
+  return revalidate(req);
 }
