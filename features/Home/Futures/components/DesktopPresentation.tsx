91637165178fedb6e36f8a38f3f7e1124eab4c8b
Andrii Shevchenko <ashevchenko0309@gmail.com>
2023-11-13 17:40:09 +0200

    rollback products links

--- features/Home/Futures/components/DesktopPresentation.tsx
+++ features/Home/Futures/components/DesktopPresentation.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 const DesktopPresentation = () => {
   return (
     <div className="hidden justify-center gap-6 px-20 lg:flex">
-      <Link href="/product/oil-free-hydrating-gel">
+      <Link href="/product/oil-free-hydrating-face-gel">
         <div className="group relative overflow-hidden">
           <Image src="https://cdn.shopify.com/s/files/1/0755/1906/6456/files/face-gel_png.webp?v=1699100906" alt="Oil-free hydrating face gel" width={258} height={429} />
           <div className="absolute left-0 top-0 h-full w-full bg-gradient-to-t from-black to-60% duration-300 group-hover:translate-y-1/2" />
@@ -21,7 +21,7 @@ const DesktopPresentation = () => {
         </div>
       </Link>
 
-      <Link href="/product/antioxidant-ginkgo-gel-booster">
+      <Link href="/product/antioxidant-face-gel-booster">
         <div className="group relative overflow-hidden">
           <Image src="https://cdn.shopify.com/s/files/1/0755/1906/6456/files/antioxidant_png_bfc6ed30-5ed9-4fbd-96a8-182873b08b11.webp?v=1699100906" alt="Antioxidant face gel booster" width={258} height={429} />
           <div className="absolute left-0 top-0 h-full w-full bg-gradient-to-t from-black to-60% duration-300 group-hover:translate-y-1/2" />
@@ -36,7 +36,7 @@ const DesktopPresentation = () => {
           </div>
         </div>
       </Link>
-      <Link href="/product/anti-age-day-cream">
+      <Link href="/product/anti-age-face-day-cream">
         <div className="group relative overflow-hidden">
           <Image src="https://cdn.shopify.com/s/files/1/0755/1906/6456/files/anti-age-face-day-cream_png_b71377bf-1485-42a3-8af9-3327cc29abb1.webp?v=1699100907" width={258} height={429} alt="Anti-age face day cream" />
           <div className="absolute left-0 top-0 h-full w-full bg-gradient-to-t from-black to-60% duration-300 group-hover:translate-y-1/2" />
@@ -51,7 +51,7 @@ const DesktopPresentation = () => {
           </div>
         </div>
       </Link>
-      <Link href="/product/purifying-mousse">
+      <Link href="/product/facial-cleanser">
         <div className="group relative overflow-hidden">
           <Image src="https://cdn.shopify.com/s/files/1/0755/1906/6456/files/facial-cleanser_png.webp?v=1699100906" alt="Facial cleanser" width={258} height={429} />
           <div className="absolute left-0 top-0 h-full w-full bg-gradient-to-t from-black to-60% duration-300 group-hover:translate-y-1/2" />
