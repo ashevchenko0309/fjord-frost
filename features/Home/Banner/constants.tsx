b0db8bda108b5fbad233deb76502a06b6512585c
Andrii Shevchenko <ashevchenko0309@gmail.com>
2023-11-14 11:31:02 +0200

    fixed banner links

--- features/Home/Banner/constants.tsx
+++ features/Home/Banner/constants.tsx
@@ -81,28 +81,28 @@ export const SECONDARY_SLIDES: BannerItemProps[][] = [
       alt: 'only men banner image',
       priority: true,
       title: 'Men only',
-      link: '/product/anti-age-face-day-cream'
+      link: '/product/anti-age-day-cream'
     },
     {
       src: "https://cdn.shopify.com/s/files/1/0755/1906/6456/files/anti-age-complex_png.webp?v=1699093898",
       alt: 'anti age complex image',
       priority: true,
       title: 'Anti-age complex',
-      link: '/product/anti-age-face-day-cream'
+      link: '/product/anti-age-day-cream'
     },
     {
       src: "https://cdn.shopify.com/s/files/1/0755/1906/6456/files/hydrading-formula_png.webp?v=1699093898",
       alt: 'hydrating formula image',
       priority: false,
       title: 'Hydrating formula',
-      link: '/product/antioxidant-face-gel-booster'
+      link: '/product/antioxidant-ginkgo-gel-booster'
     },
     {
       src: "https://cdn.shopify.com/s/files/1/0755/1906/6456/files/STUDIO_WHITE_png.webp?v=1699094174",
       alt: 'moisture boost image',
       priority: true,
       title: 'Moisture boost',
-      link: '/product/antioxidant-face-gel-booster'
+      link: '/product/antioxidant-ginkgo-gel-booster'
     }
   ],
   [
@@ -110,25 +110,25 @@ export const SECONDARY_SLIDES: BannerItemProps[][] = [
       src: "https://cdn.shopify.com/s/files/1/0755/1906/6456/files/oil-free-hydrationg-face-gel_png.webp?v=1699093897",
       alt: 'oil free hydration face gel image',
       priority: true,
-      link: '/product/oil-free-hydrating-face-gel'
+      link: '/product/oil-free-hydrating-gel'
     },
     {
       src: "https://cdn.shopify.com/s/files/1/0755/1906/6456/files/man-in-a-forest-looking-in-camera_png.webp?v=1699093897",
       alt: 'main in a forest looking in camera',
       priority: true,
-      link: '/product/oil-free-hydrating-face-gel'
+      link: '/product/oil-free-hydrating-gel'
     },
     {
       src: "https://cdn.shopify.com/s/files/1/0755/1906/6456/files/oil-free-hydrationg-face-gel-on-grass_png.webp?v=1699093898",
       alt: 'oil free hydration face gel on grass',
       priority: true,
-      link: '/product/oil-free-hydrating-face-gel'
+      link: '/product/oil-free-hydrating-gel'
     },
     {
       src: "https://cdn.shopify.com/s/files/1/0755/1906/6456/files/oil-free-hydrationg-face-gel-on-tree_png.webp?v=1699093897",
       alt: 'oil free hydration face gel on tree',
       priority: true,
-      link: '/product/oil-free-hydrating-face-gel'
+      link: '/product/oil-free-hydrating-gel'
     }
   ],
     [
@@ -136,25 +136,25 @@ export const SECONDARY_SLIDES: BannerItemProps[][] = [
         src: "https://cdn.shopify.com/s/files/1/0755/1906/6456/files/anti-age-face-day-cream_png.webp?v=1699093898",
         alt: 'anti age face day cream product',
         priority: true,
-        link: '/product/anti-age-face-day-cream'
+        link: '/product/anti-age-day-cream'
       },
       {
         src: "https://cdn.shopify.com/s/files/1/0755/1906/6456/files/man-with-earing-looking-in-camera_png.webp?v=1699093897",
         alt: 'man with earring looking in camera',
         priority: true,
-        link: '/product/anti-age-face-day-cream'
+        link: '/product/anti-age-day-cream'
       },
       {
         src: "https://cdn.shopify.com/s/files/1/0755/1906/6456/files/anti-age-face-day-cream-on-tree_png.webp?v=1699093897",
         alt: 'anti age face day cream on tree product',
         priority: true,
-        link: '/product/anti-age-face-day-cream'
+        link: '/product/anti-age-day-cream'
       },
       {
         src: "https://cdn.shopify.com/s/files/1/0755/1906/6456/files/anti-age-face-day-cream-on-grass_png.webp?v=1699093898",
         alt: 'anti age face day cream on grass product',
         priority: true,
-        link: '/product/anti-age-face-day-cream'
+        link: '/product/anti-age-day-cream'
       }
     ],
   [
@@ -162,25 +162,25 @@ export const SECONDARY_SLIDES: BannerItemProps[][] = [
       src: "https://cdn.shopify.com/s/files/1/0755/1906/6456/files/antioxidant-face-gel-booster_png.webp?v=1699093898",
       alt: 'antioxidant face gel booster product',
       priority: true,
-      link: '/product/antioxidant-face-gel-booster'
+      link: '/product/antioxidant-ginkgo-gel-booster'
     },
     {
       src: "https://cdn.shopify.com/s/files/1/0755/1906/6456/files/man-looking-in-a-sky_png.webp?v=1699093898",
       alt: 'man looking in a sky',
       priority: true,
-      link: '/product/antioxidant-face-gel-booster'
+      link: '/product/antioxidant-ginkgo-gel-booster'
     },
     {
       src: "https://cdn.shopify.com/s/files/1/0755/1906/6456/files/antioxidant-face-gel-booster-on-grass_png.webp?v=1699093897",
       alt: 'antioxidant face gel booster on grass product',
       priority: true,
-      link: '/product/antioxidant-face-gel-booster'
+      link: '/product/antioxidant-ginkgo-gel-booster'
     },
     {
       src: "https://cdn.shopify.com/s/files/1/0755/1906/6456/files/antioxidant-face-gel-booster-on-tree_png.webp?v=1699093898",
       alt: 'antioxidant face gel booster on tree product',
       priority: true,
-      link: '/product/antioxidant-face-gel-booster'
+      link: '/product/antioxidant-ginkgo-gel-booster'
     }
   ],
   [
@@ -188,25 +188,25 @@ export const SECONDARY_SLIDES: BannerItemProps[][] = [
       src: "https://cdn.shopify.com/s/files/1/0755/1906/6456/files/facial-cleaner_png.webp?v=1699093898",
       alt: 'facial cleaner product',
       priority: true,
-      link: '/product/facial-cleanser'
+      link: '/product/purifying-mousse'
     },
     {
       src: "https://cdn.shopify.com/s/files/1/0755/1906/6456/files/wet-man-looking-in-camera_png.webp?v=1699093898",
       alt: 'wet man looking in camera',
       priority: true,
-      link: '/product/facial-cleanser'
+      link: '/product/purifying-mousse'
     },
     {
       src: "https://cdn.shopify.com/s/files/1/0755/1906/6456/files/facial-cleaner-on-tree_png.webp?v=1699093898",
       alt: 'facial cleaner on tree product',
       priority: true,
-      link: '/product/facial-cleanser'
+      link: '/product/purifying-mousse'
     },
     {
       src: "https://cdn.shopify.com/s/files/1/0755/1906/6456/files/facial-cleaner-on-grass_png.webp?v=1699093898",
       alt: 'facial cleaner on grass product',
       priority: true,
-      link: '/product/facial-cleanser'
+      link: '/product/purifying-mousse'
     }
   ]
 ];
