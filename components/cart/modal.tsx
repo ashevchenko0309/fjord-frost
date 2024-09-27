3aca04123279f12a97f186e7d22f298cd84b3906
ashevchenko0309 <ashevchenko0309@gmail.com>
2024-09-27 18:01:24 +0300

    fix: remove taxes from card drawer

--- components/cart/modal.tsx
+++ components/cart/modal.tsx
@@ -179,14 +179,14 @@ export default function CartModal({ cart }: { cart: Cart | undefined }) {
                     })}
                   </ul>
                   <div className="py-4 text-sm text-neutral-100">
-                    <div className="mb-3 flex items-center justify-between border-b border-neutral-40 pb-1">
-                      <p>Taxes</p>
-                      <Price
-                        className="text-right text-base text-neutral-100"
-                        amount={cart.cost.totalTaxAmount.amount}
-                        currencyCode={cart.cost.totalTaxAmount.currencyCode}
-                      />
-                    </div>
+                    {/*<div className="mb-3 flex items-center justify-between border-b border-neutral-40 pb-1">*/}
+                    {/*  <p>Taxes</p>*/}
+                    {/*  <Price*/}
+                    {/*    className="text-right text-base text-neutral-100"*/}
+                    {/*    amount={cart.cost.totalTaxAmount.amount}*/}
+                    {/*    currencyCode={cart.cost.totalTaxAmount.currencyCode}*/}
+                    {/*  />*/}
+                    {/*</div>*/}
                     <div className="mb-3 flex items-center justify-between border-b border-neutral-40 pb-1 pt-1">
                       <p>Shipping</p>
                       <p className="text-right">Calculated at checkout</p>
